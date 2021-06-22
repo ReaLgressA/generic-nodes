a2e0ced5851db9c26c08c1a6100b6222b7b7adfd
Vlad <lugovskoy.v@gmail.com>
2021-06-22 18:01:19 +0300

    Array generic fields wip

--- Assets/Scripts/GenericNodes/Visual/GenericFields/CustomArrayGenericField.cs
+++ Assets/Scripts/GenericNodes/Visual/GenericFields/CustomArrayGenericField.cs
@@ -1,14 +1,31 @@
+using System.Collections.Generic;
 using GenericNodes.Mech.Fields;
 using GenericNodes.Visual.Interfaces;
 using GenericNodes.Visual.Nodes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GenericNodes.Visual.GenericFields {
     public class CustomArrayGenericField : MonoBehaviour, IGenericField {
         [SerializeField] private TextMeshProUGUI textLabel;
+        [SerializeField] private RectTransform rtrArrayElementsRoot;
+        [SerializeField] private Button buttonAddElement;
+        [SerializeField] private Button buttonRemoveElement;
+
+        private List<CustomObjectDataField> arrayElements = new List<CustomObjectDataField>();
         
         public GenericArrayDataField Field { get; private set; }
+
+        private void Awake() {
+            buttonAddElement.onClick.AddListener(AddNewElement);
+            buttonRemoveElement.onClick.AddListener(RemoveLastElement);
+        }
+
+        private void OnDestroy() {
+            buttonAddElement.onClick.RemoveAllListeners();
+            buttonRemoveElement.onClick.RemoveAllListeners();
+        }
 
         public void SetData(GenericArrayDataField data) {
             Field = data;
@@ -22,6 +39,14 @@ namespace GenericNodes.Visual.GenericFields {
         public void Destroy() {
             Field = null;
             GameObject.Destroy(gameObject);
+        }
+        
+        private void AddNewElement() {
+            
+        }
+        
+        private void RemoveLastElement() {
+            
         }
     }
 }
