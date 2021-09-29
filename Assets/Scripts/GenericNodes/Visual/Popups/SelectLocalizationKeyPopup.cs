c6ca219f1af45ed183a8d5191e660d1fa349ef2d
Vladyslav Lugovskoy <lugovskoy.v@gmail.com>
2021-09-29 08:55:22 +0300

    Add Select l10n key popup

---
+++ Assets/Scripts/GenericNodes/Visual/Popups/SelectLocalizationKeyPopup.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GenericNodes.Visual.Popups {
+    public class SelectLocalizationKeyPopup : MonoBehaviour {
+        [SerializeField] private RectTransform rtrListRoot;
+        [SerializeField] private Button buttonClose;
+        [SerializeField] private Button buttonApply;
+        [SerializeField] private Button buttonNewKey;
+        [SerializeField] private TMP_InputField textInputSearch;
+        [SerializeField] private TextMeshProUGUI textSelectedKey;
+    }
+}
